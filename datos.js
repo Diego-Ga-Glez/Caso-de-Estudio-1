59d3c3aa03fe996913806efe3427a7f6d479037b
Diego Garcia Gonzalez <diego.garcia2084@alumnos.udg.mx>
2021-10-17 22:23:46 -0500

    index.html

---
+++ datos.js
@@ -0,0 +1,246 @@
+let datos = [
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9789878/978987847404.JPG",
+        "titulo": "LA PLEGARIA DE LA CALLE",
+        "costo": "$369",
+        "categoria" : "Literatura",
+        "enlace" : "https://www.gonvill.com.mx/libro/la-plegaria-de-la-calle_43890004",
+        "cantidad": 1
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9789706/978970643408.JPG",
+        "titulo": "EL DIARIO DE ANA FRANK",
+        "costo": "$59",
+        "categoria" : "Infantiles",
+        "enlace" : "https://www.gonvill.com.mx/libro/diario-de-ana-frank-el_00701346",
+        "cantidad": 2
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9786071/978607110481.JPG",
+        "titulo": "DESTELLOS",
+        "costo": "$206",
+        "categoria" : "Autoayuda",
+        "enlace" : "https://www.gonvill.com.mx/libro/destellos_00102993",
+        "cantidad": 3
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9789684/978968403924.JPG",
+        "titulo": "TRAZOS Y MONITOS",
+        "costo": "$74",
+        "categoria" : "Arte",
+        "enlace" : "https://www.gonvill.com.mx/libro/trazos-y-monitos_00701089",
+        "cantidad": 4
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9786078/978607825449.JPG",
+        "titulo": "CALCULO INTEGRAL",
+        "costo": "$150",
+        "categoria" : "Ingenieria",
+        "enlace" : "https://www.gonvill.com.mx/libro/calculo-integral-bach-por-competencias_02420352",
+        "cantidad": 5
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9786070/978607077815.JPG",
+        "titulo": "A PRUEBA DE FUEGO",
+        "costo": "$398",
+        "categoria" : "Literatura",
+        "enlace" : "https://www.gonvill.com.mx/libro/a-fuego-lento_08507262",
+        "cantidad": 6
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9789706/978970643409.JPG",
+        "titulo": "EL QUIJOTE DE LA MANCHA",
+        "costo": "$59",
+        "categoria" : "Infantiles",
+        "enlace" : "https://www.gonvill.com.mx/libro/quijote-de-la-mancha-el_00701347",
+        "cantidad": 7
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9786071/978607110784.JPG",
+        "titulo": "PEQUEÑO CERDO CAPITALISTA",
+        "costo": "$256",
+        "categoria" : "Autoayuda",
+        "enlace" : "https://www.gonvill.com.mx/libro/pequeno-cerdo-capitalista_00103050",
+        "cantidad": 8
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9685848/9685848025.JPG",
+        "titulo": "DIBUJANDO ANIMALES",
+        "costo": "$172",
+        "categoria" : "Arte",
+        "enlace" : "https://www.gonvill.com.mx/libro/dibujando-animales_01350001",
+        "cantidad": 9
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9789706/978970613804.JPG",
+        "titulo": "GEOMETRIA ANALITICA DINAMICA",
+        "costo": "$301",
+        "categoria" : "Ingenieria",
+        "enlace" : "https://www.gonvill.com.mx/libro/geometria-analitica-dinamica-ccd_03202664",
+        "cantidad": 1
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9786073/978607319079.JPG",
+        "titulo": "LOS MINUTOS NEGROS",
+        "costo": "$309",
+        "categoria" : "Literatura",
+        "enlace" : "https://www.gonvill.com.mx/libro/los-minutos-negros_25607642",
+        "cantidad": 2
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9789706/978970643480.JPG",
+        "titulo": "LA ILIADA",
+        "costo": "$59",
+        "categoria" : "Infantiles",
+        "enlace" : "https://www.gonvill.com.mx/libro/iliada-la_00701398",
+        "cantidad": 3
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9786071/978607111243.JPG",
+        "titulo": "POR EL PLACER DE VIVIR",
+        "costo": "$248",
+        "categoria" : "Autoayuda",
+        "enlace" : "https://www.gonvill.com.mx/libro/por-el-placer-de-vivir_00103099",
+        "cantidad": 4
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9788489/978848943940.JPG",
+        "titulo": "LONDRES (ARQUITECTURA MODERNA)",
+        "costo": "$208",
+        "categoria" : "Arte",
+        "enlace" : "https://www.gonvill.com.mx/libro/londres-arquitectura-moderna_02780038",
+        "cantidad": 5
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9789681/978968185743.JPG",
+        "titulo": "MATEMÁTICAS DISCRETAS",
+        "costo": "$31",
+        "categoria" : "Ingenieria",
+        "enlace" : "https://www.gonvill.com.mx/libro/matematicas-discretas-conalep_04701912",
+        "cantidad": 6
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9786077/978607748271.JPG",
+        "titulo": "ARIADNA",
+        "costo": "$340",
+        "categoria" : "Literatura",
+        "enlace" : "https://www.gonvill.com.mx/libro/ariadna_16320249",
+        "cantidad": 7
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9789706/978970643526.JPG",
+        "titulo": "LA ISLA DEL TESORO",
+        "costo": "$59",
+        "categoria" : "Infantiles",
+        "enlace" : "https://www.gonvill.com.mx/libro/isla-del-tesoro-la_00701434",
+        "cantidad": 8
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9786073/978607314201.JPG",
+        "titulo": "LA MAGIA DEL ORDEN",
+        "costo": "$223",
+        "categoria" : "Autoayuda",
+        "enlace" : "https://www.gonvill.com.mx/libro/la-magia-del-orden_00103383",
+        "cantidad": 9
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9788489/978848943983.JPG",
+        "titulo": "OTTO WAGNER",
+        "costo": "$200",
+        "categoria" : "Arte",
+        "enlace" : "https://www.gonvill.com.mx/libro/otto-wagner_02780056",
+        "cantidad": 1
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9786070/978607050248.JPG",
+        "titulo": "INFORMATICA I",
+        "costo": "$145",
+        "categoria" : "Ingenieria",
+        "enlace" : "https://www.gonvill.com.mx/libro/informatica-i-por-competencias-bach-_04707450",
+        "cantidad": 2
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9786077/978607748273.JPG",
+        "titulo": "EL LIBRO DE LOS AMIGOS PERDIDOS",
+        "costo": "$370",
+        "categoria" : "Literatura",
+        "enlace" : "https://www.gonvill.com.mx/libro/libro-de-los-amigos-perdidos-el-la-historia-de-tres-jovenes-_16320248",
+        "cantidad": 3
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9789706/978970643612.JPG",
+        "titulo": "FRANKENSTEIN",
+        "costo": "$59",
+        "categoria" : "Infantiles",
+        "enlace" : "https://www.gonvill.com.mx/libro/frankenstein_00701540",
+        "cantidad": 4
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9786073/978607314850.JPG",
+        "titulo": "LIBERA TU MAGIA",
+        "costo": "$319",
+        "categoria" : "Autoayuda",
+        "enlace" : "gonvill.com.mx/libro/libera-tu-magia_00103418",
+        "cantidad": 5
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9788489/978848943927.JPG",
+        "titulo": "LOFTS",
+        "costo": "$481",
+        "categoria" : "Arte",
+        "enlace" : "https://www.gonvill.com.mx/libro/lofts_02780061",
+        "cantidad": 6
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9786071/978607150297.JPG",
+        "titulo": "CONTABILIDAD DE COSTOS 3ED",
+        "costo": "$384",
+        "categoria" : "Ingenieria",
+        "enlace" : "https://www.gonvill.com.mx/libro/contabilidad-de-costos-3ed-_05405067",
+        "cantidad": 7
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9786077/978607748272.JPG",
+        "titulo": "PRIMERO LLEGÓ EL ESCÁNDALO",
+        "costo": "$290",
+        "categoria" : "Literatura",
+        "enlace" : "https://www.gonvill.com.mx/libro/primero-llego-el-escandalo-rokesby-_16310299",
+        "cantidad": 8
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9789706/978970643879.JPG",
+        "titulo": "EL CONDE DE MONTECRISTO",
+        "costo": "$59",
+        "categoria" : "Infantiles",
+        "enlace" : "https://www.gonvill.com.mx/libro/conde-de-montecristo-el_00706879",
+        "cantidad": 9
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9786073/978607315084.JPG",
+        "titulo": "UN CLAVADO A TU CEREBRO",
+        "costo": "$281",
+        "categoria" : "Autoayuda",
+        "enlace" : "https://www.gonvill.com.mx/libro/un-clavado-a-tu-cerebro_00103429",
+        "cantidad": 1
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9788489/978848943943.JPG",
+        "titulo": "PICASSO Y SUS OBJETOS",
+        "costo": "$250",
+        "categoria" : "Arte",
+        "enlace" : "https://www.gonvill.com.mx/libro/picasso-y-sus-objetos_02780125",
+        "cantidad": 2
+    },
+    {
+        "imagen":  "https://www.gonvill.com.mx/imagenes/9701039/970103922X.JPG",
+        "titulo": "TECNICAS DE PROGRAMACION",
+        "costo": "$90",
+        "categoria" : "Ingenieria",
+        "enlace" : "https://www.gonvill.com.mx/libro/tecnicas-de-programacion_05406590",
+        "cantidad": 3
+    }
+
+];
+
+var carro  = [];
+
